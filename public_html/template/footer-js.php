d2a6aa6c1370bddf13bcd3579b83567ad4513fad
Alex Seif <alex.seif@gmail.com>
2018-10-27 06:08:18 +0200

    Nav menu sample

--- public_html/template/footer-js.php
+++ public_html/template/footer-js.php
@@ -1,28 +1,6 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 <script src="js/vendor/bootstrap.min.js"></script>
 
 <script src="js/main.js"></script>
-<script>
-  var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  })
-</script>
--- public_html/template/nav.php
+++ public_html/template/nav.php
@@ -12,6 +12,40 @@
         <img src="img/MB-word-mark_n_3Coffice.png" alt="Mercedes-Benz" height="100%" width="auto"/>
       </a>
     </div>
-    <div id="navbar" class="navbar-collapse collapse"></div><!--/.navbar-collapse -->
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="">
+          <a href="#" class="">
+            <div class="nav-icon vehicles"></div>
+            Vehicles
+          </a>
+        </li>
+        <!--        
+        <li class="">
+                  <a href="#" class="nav-link topics" data-id="432351">
+                    <span class="nav-link-text-wrapper">
+                      <span class="mbcom-icon mbcom-icon-navi-icon-theme"></span>
+                      Topics
+                    </span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="" class="mbc-brand-hub-nav-link videos" data-id="432413">
+                    <span class="nav-link-text-wrapper">
+                      <span class="mbcom-icon mbcom-icon-navi-icon-video"></span>
+                      Videos
+                    </span>
+                  </a>
+                </li>
+                <li class="depth-0 channels">
+                  <a href="#" class="mbc-brand-hub-nav-link channels" data-id="432415">
+                    <span class="nav-link-text-wrapper">
+                      <span class="mbcom-icon mbcom-icon-navi-icon-channel"></span>
+                      Channels</span>
+                  </a>
+                </li>
+        -->
+      </ul>
+    </div><!--/.navbar-collapse -->
   </div>
 </nav>
